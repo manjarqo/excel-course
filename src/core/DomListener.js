d1e48aedadc9593461e3ca81fa6a73cdcf4b6ddf
isoniasid <isoniasid@gmail.com>
2020-08-07 12:24:39 +0300

    cowork

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,20 @@
 export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error('No $root provided for DomListener')
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
 
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      // тоже что и addEventListener
+      this.$root.on(listener, () => {})
+    })
+  }
+
+  removeDOMListeners() {
+
+  }
 }
